5e765f799db2151d30c4d0ec878b028aca10d188
BRENDO KIERME <brendo.kierme@senacsp.edu.br>
2026-09-18 08:49:42 -0300

    naimicao-button

---
+++ 21-animatable-text-button/App.js
@@ -0,0 +1,59 @@
+import React, { useRef } from "react";
+import { View,  Text, StyleSheet, TouchableOpacity, Button } from "react-native";
+
+import * as Animatable from 'react-native-animatable';
+
+export default function App(){
+
+  const ButtonAnimated = Animatable.createAnimatableComponent(TouchableOpacity);
+const buttonRef = useRef(null);
+
+function handleClick(){
+  buttonRef.current.bounce()
+}
+  return(
+    <View style={styles.container}>
+      <Animatable.Text
+      style={styles.title}
+      //animation="bounce"
+      //iterationCount={3}
+      //iterationCount={infinity}
+      animation="rubberBand"
+      // iterationCount={100}
+      iterationCount={Infinity}
+      >
+        'Clique Aqui'
+      </Animatable.Text>
+      <ButtonAnimated style={styles.button} 
+      //animation="fadeInUp"
+      animation="lightSpeedIn"
+      ref={buttonRef}
+      onPress={handleClick}
+      >
+        <Text style={{color: "#fff"}}>Animar</Text>
+
+      </ButtonAnimated>
+    </View>
+  )
+
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#A855F7'
+  },
+  title: {
+    fontSize: 25
+  },
+  button: {
+    width: '70%',
+    height: 40,
+    backgroundColor: '#782121',
+    justifyContent: 'center',
+    marginTop: 15,
+    alignItems: 'center',
+    borderRadius: 20
+  }
+})
